6c8d99075d03bdc3c819310970e3183370da6ece
Herick Exterkoetter <herickherick47@gmail.com>
2023-12-20 15:11:36 -0300

    adiciona pipe para validação usando zod

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,8 +4,20 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createdAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createdAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -13,7 +25,8 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createdAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
@@ -28,11 +41,13 @@ export class CreateAccountController {
       )
     }
 
+    const hashedPasswod = await hash(password, 8)
+
     await this.prisma.user.create({
       data: {
         email,
         name,
-        password,
+        password: hashedPasswod,
       },
     })
   }
